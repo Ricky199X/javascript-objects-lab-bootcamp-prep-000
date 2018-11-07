1ba1f43462fdadcfbd0140e13baa15d9b5da4dfb
Ricky Rojas <rickyrojas19@gmail.com>
2018-11-07 21:48:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,5 @@ function updateObjectWithKeyAndValue(object, key, value) {
 function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({}, object, { [key]: value })
 }
+
+function updateObjectWithKeyAndValue()
