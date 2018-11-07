33ad5c639d2635b3ffb8b4e22e3b9609489b9fc5
Ricky Rojas <rickyrojas19@gmail.com>
2018-11-07 22:14:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function deleteFromObjectByKey(object, key) {
-  delete recipes['key']
+  delete object.key
   return object
 }
 
