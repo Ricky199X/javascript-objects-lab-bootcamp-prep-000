8e348230b0d5b2e2fe68d268fa6e77950cf4f00c
Ricky Rojas <rickyrojas19@gmail.com>
2018-11-07 22:26:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,9 +26,8 @@ function deleteFromObjectByKey(object, key) {
 }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
-  
+  delete object.key
+  return object
 }
 
-function destructivelyDeleteFromObjectByKey(object, key) {
-  
-}
+
